d70a7c10b2a8b01aaba4afe1a44d97e006ee50ae
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2024-05-03 18:09:57 +0300

    complete markup without(range)

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -6,15 +6,20 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // init Swiper:
-const swiper = new Swiper(".benefits__slider", {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  slidesPerView: 1,
-  loop: true,
-  loopedSlides: 1,
+document.addEventListener("DOMContentLoaded", () => {
+  const width = window.innerWidth;
+  if (width < 768) {
+    const swiper = new Swiper(".benefits__slider", {
+      // configure Swiper to use modules
+      modules: [Navigation, Pagination],
+      slidesPerView: 1,
+      loop: true,
+      loopedSlides: 1,
 
-  pagination: {
-    el: ".pagination",
-    clickable: true,
-  },
+      pagination: {
+        el: ".pagination",
+        clickable: true,
+      },
+    });
+  }
 });
