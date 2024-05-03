3b9391fad4a79cd106e379744005bf4ec6df93ce
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2024-05-03 14:19:45 +0300

    add section  benefits + add swiper

---
+++ src/js/swiper.js
@@ -0,0 +1,20 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const swiper = new Swiper(".benefits__slider", {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  loop: true,
+  loopedSlides: 1,
+
+  pagination: {
+    el: ".pagination",
+    clickable: true,
+  },
+});
